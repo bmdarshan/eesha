5d9ceeb9b71256bbb426df2c64df80e4c3f29ba3
Darshan Mukunda <darshan.bmg@gmail.com>
2018-09-09 01:15:25 -0400

    Figured out debugging using VS code

--- api/get_company_info.py
+++ api/get_company_info.py
@@ -2,11 +2,10 @@ from flask import jsonify
 from pymongo import MongoClient
 from flask_restful import Resource
 
-
 client = MongoClient()
 db = client.eesha
 
 class GetCompanyInfo(Resource):
   def get(self):
     companyProfile = db.company_profile.find({});
-    return jsonify({'result' : {}})
+    return jsonify({'result' : {"name": companyProfile[0]['name'], "address": companyProfile[0]['address']} })
