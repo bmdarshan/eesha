bff816fd28c59d52a31bf7e278cb107ce23f6f06
Darshan Mukunda <darshan.bmg@gmail.com>
2018-09-09 21:07:05 -0400

    test procfile

--- get_company_info.py
+++ get_company_info.py
@@ -2,8 +2,8 @@ from flask import jsonify
 from pymongo import MongoClient
 from flask_restful import Resource
 
-client = MongoClient()
-db = client.eesha
+client = MongoClient(mongodb://heroku_jdf17hjj:s77scqp3e4kvhgub6su4g5bu12@ds251002.mlab.com:51002/heroku_jdf17hjj)
+db = client.heroku_jdf17hjj
 
 class GetCompanyInfo(Resource):
   def get(self):
